0cc5e92574a1d748257bf786a9204e45c5cd83db
Cee <cee@chu2byo.com>
2014-04-25 23:36:28 +0800

    Add special time graph

--- Homework/Analyze.py
+++ Homework/Analyze.py
@@ -1,18 +1,28 @@
-__author__ = 'Cee'
-
 import urllib
 import simplejson as json
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-_URL_ = 'http://112.124.1.3:8004/api/commodity/'
+_URL_ = "http://112.124.1.3:8004/api/commodity/"
 
-def get_product_data():
-    target_url = 'http://112.124.1.3:8004/api/commodity/B00547HWBE/'
-    return json.loads(urllib.urlopen(target_url).read())
+def get_product_info(product_asin):
+    url = _URL_ + product_asin + "?field=['productInfo']"
+    jdata = json.load(urllib.urlopen(url))
+    return jdata
+
+def get_product_data(product_asin):
+    url = _URL_ + product_asin
+    jdata = json.load(urllib.urlopen(url))
+    return jdata
+
+def get_all_category():
+    url = _URL_
+    jdata = json.load(urllib.urlopen(url))
+    return [data['name'] for data in jdata]
+
 
 def price_spread():
-    productdata = get_product_data()
+    productdata = get_product_data('B00547HWBE')
     price_list = []
     for offer in productdata['offer']:
         # print(offer['timestamp'])
@@ -27,7 +37,7 @@ def price_spread():
     plt.show()
 
 def price_line():
-    product_data = get_product_data()
+    product_data = get_product_data('B00547HWBE')
 
     price_list=[]
     date_list=[]
@@ -48,7 +58,7 @@ def price_line():
     plt.show()
 
 def get_star():
-    product_data = get_product_data()
+    product_data = get_product_data('B00547HWBE')
 
     star_list = [float(single_review['star'].split()[0]) \
                  for single_review in product_data['review']]
@@ -129,9 +139,112 @@ def draw_comments_num_graph(source_data, asia_data):
     #
     # plt.show()
 
+def get_asin_in_category(category_name):
+    jdata = []
+    i = 1
+    while True:
+        url = "http://112.124.1.3:8004/api/commodity?category_name=" + category_name + "&page=%d&field=['ASIN']" % i
+        # print url
+        data = json.load(urllib.urlopen(url))
+        data = [one_data['ASIN'] for one_data in data]
+        if (len(data) >= 1):
+            jdata.extend(data)
+            i += 1
+        else:
+            break
+    return jdata
+
+
+
+def compare(special_time, time1, time2):
+    return True
+
+    time2 = datetime.strptime(time2)
+    time2 = time2.strftime("%m.%d")
+    if (special_time >= time1) & (special_time < time2):
+        return True
+    else:
+        return False
+
+
+special_date_list = [
+    '2013.01.01',
+    '2013.02.14',
+    '2013.03.14',
+    '2013.04.01',
+    '2013.05.01',
+    '2013.06.01',
+    '2013.11.01',
+    '2013.12.24',
+    '2013.12.25',
+    '2014.01.01',
+    '2014.02.14',
+    '2014.03.14',
+    '2014.04.01',
+    '2014.05.01',
+    '2014.06.01',
+    '2014.11.01',
+    '2014.12.24',
+    '2014.12.25',
+]
+special_date_list = [datetime.strptime(i, '%Y.%m.%d') for i in special_date_list]
+
+
+def prize(asin='B00547HWBE'):
+    pdata = get_product_data(asin)
+    offerdata = pdata['offer']  #   all data in offer a list
+    offerdata = sorted(offerdata, key=lambda x: datetime.strptime(x['timestamp'], '%Y-%m-%d %H:%M:%S'))
+
+    pricelist = []
+    for special_date in special_date_list:
+        flag = True
+        for i in range(len(offerdata) - 1):
+            a_offer = offerdata[i]
+            if ( special_date >= datetime.strptime(offerdata[i]['timestamp'], '%Y-%m-%d %H:%M:%S') ) & (
+                        special_date < datetime.strptime(offerdata[i + 1]['timestamp'], '%Y-%m-%d %H:%M:%S') ):
+                if len(a_offer['info']) >= 1:
+                    pricelist.append(float(a_offer['info'][0]['price']))
+                else:
+                    pricelist.append(None)
+                flag = False
+            break
+        if flag:
+            pricelist.append(None)
+
+    print len(special_date_list)
+    print len(pricelist)
+
+    plt.plot(special_date_list, pricelist, 'ro')
+    plt.gcf().autofmt_xdate()
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.show()
+
+
+def review(asin='B0099HX6HY'):
+    pdata = get_product_data(asin)
+    reviewdata = pdata['review']
+    reviewdata.sort(key=lambda x: datetime.strptime(x['publishTime'], '%Y-%m-%d %H:%M:%S'))
+    count_list = []
+    for special_date in special_date_list:
+        count = 0;
+        for a_review in reviewdata:
+            if special_date.strftime('%Y%m%d') == datetime.strptime(a_review['publishTime'],
+                                                                    '%Y-%m-%d %H:%M:%S').strftime('%Y%m%d'):
+                #do some thing
+                count += 1
+        count_list.append(count)
+    plt.plot(special_date_list, count_list, 'ro-')
+    plt.gcf().autofmt_xdate()
+    plt.xlabel('Time')
+    plt.ylabel('Review Count')
+    plt.show()
+
 if __name__ == '__main__':
     get_star()
-    price_line()
+    # price_line()
     price_spread()
     draw_comments_num_graph(get_reviewNum(get_ASIN()), get_ASIN())
+    prize()
+    review()
     pass
